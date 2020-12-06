359f104a18bff5af605e2942f46d735d75df620a
Sherlock669 <learningstuffs4@gmail.com>
2020-12-06 18:28:04 +0400

    adding pizza and burger

--- hungry.py
+++ hungry.py
@@ -2,6 +2,8 @@ hungry=input("Are you hungry ? ")
 if(hungry.lower()=='yes'):
   print("Eat KFC!")
   print("Eat fries!")
+  print("Eat burger!")
+  print("Eat pizza!")
 else:
   thirsty = input("Are you thirsty ? ")
   if (thirsty.lower() == 'yes'):
